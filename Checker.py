2694da9d7877617d007ffd7f331f4a0fa5f14a99
LSGio <shvartzlior@gmail.com>
2023-01-01 00:56:54 +0200

    FORMAT argument removed and added a flag instead rewrote some logic in constructor and deleted redundant methods
    still need to implement __print_result()

--- Checker.py
+++ Checker.py
@@ -6,20 +6,10 @@ import sys
 
 class Checker:
 
-    USAGE = "Usage : python3 Main.py <password-string> [FORMAT] [FLAGS]...\n" \
+    USAGE = "Usage : python3 Main.py <password-string> [FLAGS]...\n" \
             "\n" \
             "The script will compare the given password and check if it exists\n" \
             "somewhere inside the lists of previously used \\ leaked \\ common credentials\n" \
-            "\n" \
-            "FORMAT determines the format of the output\n" \
-            "\n" \
-            "Valid Formats : \n" \
-            "\n" \
-            "1 (default) - print only binary answer after the scan finishes, \n" \
-            "either the password exists or it doesn't exist\n" \
-            "2 - print names of files during the scan\n" \
-            "3 - print names of files and number of occurrences during the scan \n" \
-            "FLAGS affect the behaviour of the script, more info below (may affect scan speeds)\n" \
             "\n" \
             "Valid Flags : \n" \
             "\n" \
@@ -29,73 +19,63 @@ class Checker:
             "-p, --progress\n" \
             "\t show progress bar during the scan" \
             "\n" \
+            "-c, --count\n" \
+            "\t print the number of occurrences for the given password" \
             "Notes : \n" \
             "\n" \
+            "* Flags should be separated by space, no support for grouped flags yet\n" \
             "* Line endings other than LF are not supported yet\n" \
             "* Encodings other than UTF-8 are not supported yet\n"
 
-    VALID_FORMATS = ("1", "2", "3")
+    VALID_COUNT_FLAGS = ("-c", "--count")
     VALID_VERBOSE_FLAGS = ("-v", "--verbose")
     VALID_PROGRESS_FLAGS = ("-p", "--progress")
 
-    # Exit codes and error codes
+    # Error codes
 
-    EXIT_CODE_PASSWORD_NOT_IN_LISTS = 0
-    EXIT_CODE_PASSWORD_IN_LISTS = 1
-    ERROR_CODE_NOT_ENOUGH_ARGS = 2
-    ERROR_CODE_INVALID_FORMAT = 3
+    ERROR_CODE_NOT_ENOUGH_ARGS = -1
 
-    # Corresponding exit and error messages
+    # Corresponding error messages
 
     EXIT_MESSAGES = (
-        "Your password is not in the lists",
-        "Your password is in the lists",
-        f"Missing required positional argument at position 1 \n\n{USAGE}",
-        f"Invalid format, must be one of : {VALID_FORMATS}"
+        f"Missing required positional argument at position 1 \n\n{USAGE}"
     )
 
-    def __init__(self, credential: str = None, output_format: str = "1", flags: tuple = None):
+    def __init__(self, credential: str = None, flags: tuple = None):
 
         # Mandatory attributes
-        self.__credential = self.__get_valid_credential(credential)
-        self.__output_format = self.__get_valid_output_format(output_format)
-
-        # Optional attributes
-        self.__credential_occurrences = 0
-        self.__progress = 0
-        self.__credential_found = False
-        self.__flags = () if flags is None else flags
-        self.__verbose = self.__is_verbose()
-        self.__show_progress = self.__should_show_progress()
-
-    def __get_valid_credential(self, credential: str) -> str:
-
         if credential is None:
             self.__handle_error_or_exit_with_code(Checker.ERROR_CODE_NOT_ENOUGH_ARGS)
         else:
-            return credential
+            self.__credential = credential
 
-    def __get_valid_output_format(self, output_format: str) -> int:
-
-        if output_format not in Checker.VALID_FORMATS:
-            self.__handle_error_or_exit_with_code(Checker.ERROR_CODE_INVALID_FORMAT)
-        else:
-            return int(output_format)
-
-    def __is_verbose(self) -> bool:
-
+        # Check flags
+        self.__flags = () if flags is None else flags
+        for flag in Checker.VALID_COUNT_FLAGS:
+            if flag in self.__flags:
+                self.__print_count = True
+        self.__print_count = False
+        for flag in Checker.VALID_PROGRESS_FLAGS:
+            if flag in self.__flags:
+                self.__show_progress = True
+        self.__show_progress = False
         for flag in Checker.VALID_VERBOSE_FLAGS:
             if flag in self.__flags:
-                return True
-        return False
+                self.__verbose = True
+        self.__verbose = False
+
+        # Internal variables
+        self.__credential_occurrences = 0
+        self.__progress = 0
+        self.__credential_found = False
 
     def __should_show_progress(self) -> bool:
 
         for flag in Checker.VALID_PROGRESS_FLAGS:
             if flag in self.__flags:
                 if sys.stdout.encoding != "utf-8":
+                    # Try to get the utf-8 writer
                     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.buffer, 'strict')
-                if sys.stderr.encoding != "utf-8":
                     sys.stderr = codecs.getwriter("utf-8")(sys.stderr.buffer, 'strict')
                 return True
         return False
@@ -105,7 +85,8 @@ class Checker:
         if not isinstance(code, int):
             raise TypeError(f"Expected exit code must be of type 'int' but got : {type(code)}")
         else:
-            print(f"\r\n{Checker.EXIT_MESSAGES[code]}")
+            print(f"\033[1K\r{Checker.EXIT_MESSAGES[code]}")
+            self.__print_progress()
             exit(code)
 
     def __print_verbose(self, message: str):
@@ -124,10 +105,13 @@ class Checker:
             percent = '█' * self.__progress + '_' * (100 - self.__progress)
             print(f"\033[1K\r[{percent}] {self.__progress}%", end="")
 
+    def __print_result(self):
+        pass
+
     def check(self) -> None:
 
-        cwd = os.getcwd()
-        filenames = glob.glob("**/*.txt", root_dir=os.getcwd(), recursive=True)
+        current_directory = os.getcwd()
+        filenames = glob.glob("**/*.txt", root_dir=current_directory, recursive=True)
         number_of_files = len(filenames)
 
         for index, filename in enumerate(filenames):
@@ -136,18 +120,13 @@ class Checker:
             self.__update_progress(index + 1, number_of_files)
             self.__print_progress()
 
-            full_name = os.path.join(cwd, filename)
+            full_name = os.path.join(current_directory, filename)
             self.__print_verbose(f"Checking file : {full_name}")
             with open(full_name, encoding='utf-8') as current_file:
                 for line in current_file:
-                    line = line.strip('\n')
+                    line = line.strip()
                     if line == self.__credential:
                         self.__credential_occurrences += 1
                         self.__credential_found = True
 
-        if not self.__credential_found:
-            self.__handle_error_or_exit_with_code(Checker.EXIT_CODE_PASSWORD_NOT_IN_LISTS)
-        else:
-            if self.__output_format == 2:
-                print(f"We counted {self.__credential_occurrences} occurrences of your password")
-            self.__handle_error_or_exit_with_code(Checker.EXIT_CODE_PASSWORD_IN_LISTS)
+        self.__print_result()
--- Main.py
+++ Main.py
@@ -6,20 +6,16 @@ from Checker import Checker
 def main():
 
     if len(sys.argv) < 2:
-        # Password not provided by user, checking empty string
+        # Password not provided by user
         C = Checker()
         C.check()
     elif len(sys.argv) == 2:
-        # Output format not provided by user
+        # Flags not provided by user
         C = Checker(sys.argv[1])
         C.check()
-    elif len(sys.argv) == 3:
-        # Output format was provided
-        C = Checker(sys.argv[1], sys.argv[2])
-        C.check()
-    elif len(sys.argv) >= 4:
+    elif len(sys.argv) >= 3:
         # Flags were provided
-        C = Checker(sys.argv[1], sys.argv[2], tuple(sys.argv[3::]))
+        C = Checker(sys.argv[1], tuple(sys.argv[2::]))
         C.check()
 
 
