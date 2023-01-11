4461356f808e773a025930a133ba920bd3e7590c
LSGio <shvartzlior@gmail.com>
2023-01-11 23:01:12 +0200

    mostly refactoring verbose mode now puts an asterisk next to a file if credential is found there

--- Checker.py
+++ Checker.py
@@ -7,7 +7,7 @@ class Checker:
 
     USAGE = "Usage : python3 Main.py [FLAGS]...\n" \
             "\n" \
-            "The script will compare the given password and check if it exists\n" \
+            "The script compares a user-given password and checks if it exists\n" \
             "somewhere inside the lists of previously used \\ leaked \\ common credentials\n" \
             "\n" \
             "Valid Flags : \n" \
@@ -18,7 +18,7 @@ class Checker:
             "-i, --insecure\n" \
             "\t echo the password to the console\n" \
             "-v, --verbose\n" \
-            "\t print additional information to the console\n" \
+            "\t print scanned filenames, add an asterisk if credential is found there\n" \
             "\n" \
             "-c, --count\n" \
             "\t print the number of occurrences for the given password\n" \
@@ -37,7 +37,13 @@ class Checker:
 
     PASSWORD_PROMPT = "Please provide a credential: "
 
+    RESULT_PASSWORD_FOUND = "Your credential is in the files!"
+    RESULT_PASSWORD_NOT_FOUND = "Your credential is not in the files!"
+    RESULT_OCCURRENCE_FORMAT = "We counted {0} occurrences of your password"
+
     EXIT_CODE_OK = 0
+
+    ERROR_TYPE_MISMATCH = "Expected an exit code of type {0} but got : {1} instead"
 
     def __init__(self, flags: tuple | None = None):
 
@@ -70,14 +76,14 @@ class Checker:
         self.__credential_occurrences = 0
         self.__credential_found = False
 
-    def __print_error_and_exit_with_code(self, code: int, message: str) -> None:
+    def __print_error_and_exit_with_code(self, code: int = EXIT_CODE_OK, message: str | None = None) -> None:
 
         if not isinstance(code, int):
-            raise TypeError(f"Expected an exit code of type 'int' but got : {type(code)} instead")
+            raise TypeError(Checker.ERROR_TYPE_MISMATCH.format(str(int), str(type(code))))
         if not isinstance(message, str):
-            raise TypeError(f"Expected an error message of type 'str' but got : {type(message)} instead")
-
-        print(message)
+            raise TypeError(Checker.ERROR_TYPE_MISMATCH.format(str(str), str(type(message))))
+        if message is not None:
+            print(message)
         exit(-1 * code)
 
     def __print_verbose(self, message: str):
@@ -87,27 +93,35 @@ class Checker:
 
     def __print_result(self):
 
-        print()
-        print()
-        print(f"Password found : {self.__credential_found}")
-        if self.__print_count:
-            print(f"We counted {self.__credential_occurrences} occurrences of your password")
+        result = Checker.RESULT_PASSWORD_FOUND if self.__credential_found else Checker.RESULT_PASSWORD_NOT_FOUND
+        print(result)
+        if self.__print_count and self.__credential_found:
+            print(Checker.RESULT_OCCURRENCE_FORMAT.format(self.__credential_occurrences))
 
     def check(self) -> None:
 
+        glob_pattern_passwords = "Passwords/**/*.txt"
+        glob_pattern_hashes = "Hashes/**/*.txt"
+        glob_pattern_login_pairs = "LoginPairs/**/*.txt"
+
         current_directory = os.getcwd()
-        filenames = glob.glob("**/*.txt", root_dir=current_directory, recursive=True)
-        number_of_files = len(filenames)
+        filenames = glob.glob(glob_pattern_passwords, root_dir=current_directory, recursive=True)
+        self.__files_dict = dict.fromkeys(filenames, 0)
 
         for index, filename in enumerate(filenames):
 
             full_name = os.path.join(current_directory, filename)
-            self.__print_verbose(f"Checking file : {full_name}")
             with open(full_name, encoding='utf-8') as current_file:
                 for line in current_file:
                     line = line.strip()
                     if line == self.__credential:
-                        self.__credential_occurrences += 1
+                        self.__files_dict[filename] += 1
                         self.__credential_found = True
 
+                if self.__files_dict[filename] != 0:
+                    self.__print_verbose(f"File : {full_name} *")
+                else:
+                    self.__print_verbose(f"File : {full_name}")
+
+        self.__credential_occurrences = sum(self.__files_dict.values())
         self.__print_result()
